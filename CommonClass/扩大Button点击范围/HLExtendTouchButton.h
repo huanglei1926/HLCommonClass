98a3359a7769621401c0aed2de424d42f6d06e54
Len <huanglei1926@gmail.com>
2019-03-26 17:13:38 +0800

    扩大Button点击范围
    扩大Button点击范围

---
+++ CommonClass/扩大Button点击范围/HLExtendTouchButton.h
@@ -0,0 +1,18 @@
+//
+//  HLExtendTouchButton.h
+//  HLExtendTouchButton
+//
+//  Created by Len on 2019/3/14.
+//  Copyright © 2019 HLExtendTouchButton. All rights reserved.
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface HLExtendTouchButton : UIButton
+
+@property (nonatomic, assign) CGFloat touchAreaValue;
+
+@end
+
+NS_ASSUME_NONNULL_END
